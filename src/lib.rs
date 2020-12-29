c205323defa310732b3463a6dcc5bce54955510a
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2020-12-29 19:56:33 +0100

    added footer and history

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ struct Model {
     cmds: Vec<Command>,
     calc: Calculator,
     current_command: String,
+    history: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -22,10 +23,47 @@ pub enum Message {
     CommandUpdate(String),
     ExecuteCommand,
     ClearCommand,
+    HistoryUp,
+    HistoryDown,
 }
 
 const ENTER_KEY: &str = "Enter";
 const ESC_KEY: &str = "Escape";
+const UP_KEY: &str = "ArrowUp";
+const DOWN_KEY: &str = "ArrowDown";
+
+fn view_footer() -> Node<Message> {
+    const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+    const REPO: Option<&'static str> = option_env!("CARGO_PKG_REPOSITORY");
+
+    div![
+        C!["row"],
+        div![
+            C!["col footer"],
+            "Calculator",
+            if let Some(url) = REPO {
+                span![
+                    " | ",
+                    a![
+                        attrs! {
+                            At::Href => url;
+                            At::Rel => "norefferer noopener external";
+                            At::Target => "_blank"
+                        },
+                        "Github"
+                    ],
+                ]
+            } else {
+                seed::empty()
+            },
+            format!(
+                " | {}: {}",
+                "version",
+                VERSION.unwrap_or("<version unknown>")
+            )
+        ]
+    ]
+}
 
 fn view(model: &Model) -> Node<Message> {
     let mut commands: Vec<Node<Message>> = model
@@ -33,12 +71,12 @@ fn view(model: &Model) -> Node<Message> {
         .iter()
         .map(|cmd| {
             let res = match &cmd.res {
-                Ok(Some(num)) => div![C!("col-12 success"), num.to_string()],
+                Ok(Some(num)) => div![C!("col-12 success"), "=> ", num.to_string()],
                 Ok(None) => seed::empty(),
-                Err(err) => div![C!("col-12 failure"), format!("Error: {:?}", err)],
+                Err(err) => div![C!("col-12 failure"), pre![format!("Error: {:}", err)]],
             };
             vec![
-                div![C!("row"), div![C!("col-12"), cmd.cmd.clone()],],
+                div![C!("row"), div![C!("col-12"), span![C!("prompt"), "> "], cmd.cmd.to_string()],],
                 div![C!("row"), res],
             ]
         })
@@ -55,6 +93,7 @@ fn view(model: &Model) -> Node<Message> {
                     At::Name => "command",
                     At::Placeholder => "command",
                     At::AutoFocus => true.as_at_value(),
+                    At::Value => model.current_command,
                     "aria-label" => "Command",
                     "aria-describedby" => "basic-addon2",
                 ],
@@ -63,6 +102,8 @@ fn view(model: &Model) -> Node<Message> {
                     Some(match keyboard_event.key().as_str() {
                         ENTER_KEY => Message::ExecuteCommand,
                         ESC_KEY => Message::ClearCommand,
+                        UP_KEY => Message::HistoryUp,
+                        DOWN_KEY => Message::HistoryDown,
                         _ => return None,
                     })
                 }),
@@ -78,7 +119,13 @@ fn view(model: &Model) -> Node<Message> {
             ]
         ]
     ]);
-    div![C!("container"), commands]
+
+    div![
+        C!("container"),
+        div![C!("col-12"), h1!("Calculator")],
+        commands,
+        view_footer()
+    ]
 }
 
 fn update(message: Message, model: &mut Model, _: &mut impl Orders<Message>) {
@@ -93,8 +140,31 @@ fn update(message: Message, model: &mut Model, _: &mut impl Orders<Message>) {
                 cmd: model.current_command.clone(),
                 res,
             });
+            model.history = model.cmds.len();
             model.current_command.clear();
-        }
+        },
+        Message::HistoryDown => { 
+            let mut history_entry = model.history;
+            if history_entry < model.cmds.len() {
+                history_entry += 1;
+            }
+            model.history = history_entry;
+            if model.history < model.cmds.len() {
+                model.current_command = model.cmds[model.history].cmd.clone();
+            } else {
+                model.current_command.clear();
+            }
+        },
+        Message::HistoryUp => {
+            let mut history_entry = model.history;
+            if history_entry > 0 {
+                history_entry -= 1;
+            }
+            model.history = history_entry;
+            if model.history < model.cmds.len() {
+                model.current_command = model.cmds[model.history].cmd.clone();
+            }
+        },
     }
 }
 
